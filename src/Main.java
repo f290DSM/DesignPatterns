9888090fe9a40b6f72e0e97f5256b1f88d619b79
Esdras Bezerra da Silva <BZR4@users.noreply.github.com>
2026-08-12 21:20:22 -0300

    Enum vs Magic Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,77 @@
+import domain.StrategyType;
+
 import java.util.Random;
 import java.util.Scanner;
+import static domain.StrategyType.*;
 
 public class Main {
     // Remoção de números mágicos
-    private static final Integer PAPEL = 1;
-    private static final Integer TESOURA = 2;
-    private static final Integer PEDRA = 3;
-    private static final Integer LAGARTO = 4;
-    private static final Integer SPOCK = 5;
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Escolha uma das opções [ 1-PAPEL, 2-TESOURA, 3-PEDRA, 4-LAGARTO, 5-SPOCK ]\n");
-        int jogada = in.nextInt();
-        int computador = new Random().nextInt(5) + 1;
-        System.out.println("O COMPUTADOR escolheu: " + computador);
+        int player = in.nextInt();
+        StrategyType strategyPlayer = StrategyType.getById(player);
+        int computer = new Random().nextInt(5) + 1;
+        StrategyType strategyComputer = StrategyType.getById(computer);
+        System.out.println("O COMPUTADOR escolheu: " + strategyComputer);
+        System.out.println("O JOGADOR escolheu: " + strategyPlayer);
 
-        if (jogada == PAPEL && computador == PAPEL) {
+
+        if (strategyPlayer == PAPEL && strategyComputer == PAPEL) {
             System.out.println("Empate. Papel empata com papel!");
-        } else if (jogada == PAPEL && computador == TESOURA) {
+        } else if (strategyPlayer == PAPEL && strategyComputer == TESOURA) {
             System.out.println("Perdeu! Tesoura corta o papel!");
-        } else if (jogada == PAPEL && computador == PEDRA) {
+        } else if (strategyPlayer == PAPEL && strategyComputer == PEDRA) {
             System.out.println("Ganhou! Pedra embrulha o papel!");
-        } else if (jogada == PAPEL && computador == LAGARTO) {
+        } else if (strategyPlayer == PAPEL && strategyComputer == LAGARTO) {
             System.out.println("Perdeu! Lagarto come o papel!");
         }
 
-        else if (jogada == PAPEL) {
+        else if (strategyPlayer == PAPEL) {
             System.out.println("Ganhou! Papel refuta o Spock!");
-        } else if (jogada == TESOURA && computador == PAPEL) {
+        } else if (strategyPlayer == TESOURA && strategyComputer == PAPEL) {
             System.out.println("Ganhou. Tesoura corta o papel!");
-        } else if (jogada == TESOURA && computador == TESOURA) {
+        } else if (strategyPlayer == TESOURA && strategyComputer == TESOURA) {
             System.out.println("Empate! Tesoura empata com tesoura!");
-        } else if (jogada == TESOURA && computador == PEDRA) {
+        } else if (strategyPlayer == TESOURA && strategyComputer == PEDRA) {
             System.out.println("Perdeu! Pedra quebra a tesoura!");
-        } else if (jogada == TESOURA && computador == LAGARTO) {
+        } else if (strategyPlayer == TESOURA && strategyComputer == LAGARTO) {
             System.out.println("Ganhou! Tesoura decapta o lagarto!");
         }
 
-        else if (jogada == TESOURA) {
+        else if (strategyPlayer == TESOURA) {
             System.out.println("Perdeu! Spock derrete a tesoura!");
-        } else if (jogada == PEDRA && computador == PAPEL) {
+        } else if (strategyPlayer == PEDRA && strategyComputer == PAPEL) {
             System.out.println("Perdeu. Papel embrulha a pedar!");
-        } else if (jogada == PEDRA && computador == TESOURA) {
+        } else if (strategyPlayer == PEDRA && strategyComputer == TESOURA) {
             System.out.println("Empate! Tesoura empata com tesoura!");
-        } else if (jogada == PEDRA && computador == PEDRA) {
+        } else if (strategyPlayer == PEDRA && strategyComputer == PEDRA) {
             System.out.println("Empatou! Pedra empata com pedra!");
-        } else if (jogada == PEDRA && computador == LAGARTO) {
+        } else if (strategyPlayer == PEDRA && strategyComputer == LAGARTO) {
             System.out.println("Ganhou! Pedra esmaga o lagarto!");
-        } else if (jogada == PEDRA) {
+        } else if (strategyPlayer == PEDRA) {
             System.out.println("Perdeu! Spock vaporiza a apedra!");
-        } else if (jogada == LAGARTO && computador == PAPEL) {
+        } else if (strategyPlayer == LAGARTO && strategyComputer == PAPEL) {
             System.out.println("Ganhou. Lagarto come papel!");
-        } else if (jogada == LAGARTO && computador == TESOURA) {
+        } else if (strategyPlayer == LAGARTO && strategyComputer == TESOURA) {
             System.out.println("Perdeu! Tesoura decapta o lagarto!");
-        } else if (jogada == LAGARTO && computador == PEDRA) {
+        } else if (strategyPlayer == LAGARTO && strategyComputer == PEDRA) {
             System.out.println("Perdeu! Pedra esmaga lagarto!");
-        } else if (jogada == LAGARTO && computador == LAGARTO) {
+        } else if (strategyPlayer == LAGARTO && strategyComputer == LAGARTO) {
             System.out.println("Empatou! Lagarto empata com lagarto!");
-        } else if (jogada == LAGARTO) {
+        } else if (strategyPlayer == LAGARTO) {
             System.out.println("Ganhou! Lagarto envenena o Spock!");
-        } else if (jogada == SPOCK && computador == PAPEL) {
+        } else if (strategyPlayer == SPOCK && strategyComputer == PAPEL) {
             System.out.println("Perdeu. Papel refuta Spock!");
-        } else if (jogada == SPOCK && computador == TESOURA) {
+        } else if (strategyPlayer == SPOCK && strategyComputer == TESOURA) {
             System.out.println("Ganhou! Spock derrete a tesoura!");
-        } else if (jogada == SPOCK && computador == PEDRA) {
+        } else if (strategyPlayer == SPOCK && strategyComputer == PEDRA) {
             System.out.println("Ganhou! Spock vaporiza a pedra!");
-        } else if (jogada == SPOCK && computador == LAGARTO) {
+        } else if (strategyPlayer == SPOCK && strategyComputer == LAGARTO) {
             System.out.println("Perdeu! Lagarto envenena Spock!");
-        } else if (jogada == SPOCK) {
+        } else if (strategyPlayer == SPOCK) {
             System.out.println("Empatou! Spock empata com Spock!");
         }
 
